a6b2dcb18b98ae317c63b87e0aaed21813f8e1a3
57383 <jasonvinda@gmail.com>
2022-05-10 15:15:27 +0200

    projet-planer with rayhan

---
+++ script.js
@@ -0,0 +1,95 @@
+
+const text = document.getElementById("text");
+const date = document.getElementById("party");
+const addTaskButton = document.getElementById("add-task-btn");
+const saveTaskButton = document.getElementById("save-todo-btn");
+const listBox = document.getElementById("listBox");
+const saveInd = document.getElementById("saveIndex");
+
+
+let todoArray = [];
+
+addTaskButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let todo = localStorage.getItem("todo");
+    if (todo === null) {
+        todoArray = [];
+    } else {
+        todoArray = JSON.parse(todo);
+    }
+    todoArray.push(`${text.value} ${date.value}`);
+    text.value = " ";
+    date.value= "";
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+});
+
+function displayTodo() {
+    let todo = localStorage.getItem("todo");
+    if (todo === null) {
+        todoArray = [];
+    } else {
+        todoArray = JSON.parse(todo);
+    }
+    let htmlCode = "";
+    todoArray.forEach((list, ind) => {
+        htmlCode += `<div class='flex mb-4 items-center'>
+      <p class='w-full text-grey-darkest'>${list}</p>
+      <button onclick='edit(${ind})' class='flex-no-shrink p-2 ml-4 mr-2 border-2 rounded text-white text-grey bg-green-600'>Edit</button>
+      <button onclick='deleteTodo(${ind})' class='flex-no-shrink p-2 ml-2 border-2 rounded text-white bg-red-500'>Delete</button>
+      </div>`;
+    });
+    listBox.innerHTML = htmlCode;
+}
+
+function deleteTodo(ind) {
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    todoArray.splice(ind, 1);
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+}
+
+function edit(ind) {
+    saveInd.value = ind;
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    text.value = todoArray[ind];
+    addTaskButton.style.display = "none";
+    saveTaskButton.style.display = "block";
+}
+
+saveTaskButton.addEventListener("click", () => {
+    let todo = localStorage.getItem("todo");
+    todoArray = JSON.parse(todo);
+    let id = saveInd.value;
+    todoArray[id] = text.value;
+    addTaskButton.style.display = "block";
+    saveTaskButton.style.display = "none";
+    text.value = "";
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+});
+
+//horloge
+clock2();
+
+function clock2() {
+    const date = new Date();
+    const hours = ((date.getHours() + 11) % 12 + 1);
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
+
+    document.querySelector('.heure').style.transform = `rotate(${hour}deg)`;
+
+    document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
+
+    document.querySelector('.seconde').style.transform = `rotate(${second}deg)`;
+}
+
+setInterval(clock2, 1000);
+
+
